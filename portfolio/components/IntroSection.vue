16c8189b3b3312fe5a0d2dd44f5deb39e73a55f7
YeJun, Jung <yejunjung98@gmail.com>
2022-03-13 23:50:39 +0900

    Add animation of change background and border color in typing

--- portfolio/components/IntroSection.vue
+++ portfolio/components/IntroSection.vue
@@ -72,6 +72,18 @@ export default {
     to { opacity: 1; }
   }
 
+  @keyframes change-frame-color {
+    from {
+      background: rgba(255, 255, 255, 0.2);
+      border: 1px solid rgba(255, 255, 255, 0.3);
+    }
+
+    to {
+      background: rgba(0, 0, 0, 0.2);
+      border: 1px solid rgba(0, 0, 0, 0.3);
+    }
+  }
+
   #intro-section {
     width: 100%;
     height: 100%;
@@ -140,6 +152,8 @@ export default {
     backdrop-filter: blur(10px);
     -webkit-backdrop-filter: blur(10px);
     border: 1px solid rgba(255, 255, 255, 0.3);
+
+    animation: change-frame-color 1s forwards 4s;
   }
 
   #welcome-typing-title {
